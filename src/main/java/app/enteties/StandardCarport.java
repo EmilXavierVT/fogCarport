d0557d13f45266255b4d1617fca2fb5e7cd56914
frede <frededv@outlook.dk>
2025-11-19 10:58:34 +0000

    StandardCarport entetie created

---
+++ src/main/java/app/enteties/StandardCarport.java
@@ -0,0 +1,8 @@
+package app.enteties;
+
+import java.time.LocalDate;
+
+public class StandardCarport {
+
+    public StandardCarport(int standardId, String name, int price, int type, String description, int specification, String pdf_file) {}
+}

4b7a4d6cdc0a8416148c2a5e4898625ee34ac5e3
frede <frededv@outlook.dk>
2025-11-19 10:09:16 +0000

    User class created

---
+++ src/main/java/app/enteties/User.java
@@ -0,0 +1,6 @@
+package app.enteties;
+
+public class User {
+
+    public User(int userId, String firstName, String lastName, int zip_code, String streetName, int houseNumber, String floor, String email, String password,int role) {}
+}

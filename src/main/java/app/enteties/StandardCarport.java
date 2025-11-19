4e9263e68e60eb355fc920b6bfbdff552fa5f361
frede <frededv@outlook.dk>
2025-11-19 13:12:16 +0000

    StandardCarport entetie created

--- src/main/java/app/enteties/StandardCarport.java
+++ src/main/java/app/enteties/StandardCarport.java
@@ -3,6 +3,20 @@ package app.enteties;
 import java.time.LocalDate;
 
 public class StandardCarport {
-
-    public StandardCarport(int standardId, String name, int price, int type, String description, int specification, String pdf_file) {}
+int standardId;
+String name;
+int price;
+int type;
+String description;
+int specification;
+String pdfFile;
+    public StandardCarport(int standardId, String name, int price, int type, String description, int specification, String pdf_file) {
+        this.standardId = standardId;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.description = description;
+        this.specification = specification;
+        this.pdfFile = pdf_file;
+    }
 }

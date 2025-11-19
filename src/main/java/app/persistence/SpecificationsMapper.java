2238cd7ac5f8ba07c9f51452818c14e117a32709
frede <frededv@outlook.dk>
2025-11-19 13:40:41 +0000

    SpecificationMapper added functions implimented: createSpecification() getSpecification()

--- src/main/java/app/persistence/SpecificationsMapper.java
+++ src/main/java/app/persistence/SpecificationsMapper.java
@@ -2,22 +2,24 @@ package app.persistence;
 
 
 import app.enteties.Specifications;
+import app.exceptions.DatabaseException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SpecificationsMapper {
 
     public static void CreateSpecification(int EAN, String model, String roomFor,boolean shed, int post,
                                            int beam, int rafter, int roof, int fasciaBoard, int length, int width, int heightFront,
-                                           int heightRear, int roofLength, int roofWidth, int exteriorWidthAtPost, int parkingWidth,
-                                           int shedDepth, int shedWidth) throws SQLException {
+                                           int heightRear, int roofLength, int roofWidth, int exteriorWidthAtPost, int parkingLength,
+                                           int parkingWidth, int shedDepth, int shedWidth) throws SQLException {
         ConnectionPool connectionPool = ConnectionPool.getInstance();
         String sql = "INSERT INTO specifications (EAN,model.room_for,shed,post,beam,rafter, roof, " +
                 "fascia_board, length, width, height_front, height_rear, roof_length, roof_width," +
-                " exterior_width_at_post, parking_length, parking_width, shed_depth, shed_width)" +
-                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                " exterior_width_at_post, parking_length, parking_width,  shed_depth, shed_width)" +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         try (Connection connection = connectionPool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql))
         {
@@ -37,14 +39,54 @@ public class SpecificationsMapper {
             ps.setInt(14, roofLength);
             ps.setInt(15, roofWidth);
             ps.setInt(16, exteriorWidthAtPost);
-            ps.setInt(17, parkingWidth);
-            ps.setInt(18, shedDepth);
-            ps.setInt(19, shedWidth);
+            ps.setInt(17, parkingLength);
+            ps.setInt(18, parkingWidth);
+            ps.setInt(19, shedDepth);
+            ps.setInt(20, shedWidth);
             ps.executeUpdate();
         }
     }
 
-    public static Specifications GetSpecifications(int specificationId) throws SQLException {
+    public static Specifications GetSpecifications(int specificationId) throws SQLException, DatabaseException {
+        ConnectionPool connectionPool = ConnectionPool.getInstance();
+        String sql = "SELECT * FROM specifications WHERE specification_id = ?";
 
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, specificationId);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new Specifications(
+                        rs.getInt("specification_id"),
+                        rs.getInt("EAN"),
+                        rs.getString("model"),
+                        rs.getString("room_for"),
+                        rs.getBoolean("shed"),
+                        rs.getInt("post"),
+                        rs.getInt("beam"),
+                        rs.getInt("rafter"),
+                        rs.getInt("roof"),
+                        rs.getInt("fascia_board"),
+                        rs.getInt("length"),
+                        rs.getInt("width"),
+                        rs.getInt("height_front"),
+                        rs.getInt("height_rear"),
+                        rs.getInt("roof_length"),
+                        rs.getInt("roof_width"),
+                        rs.getInt("exterior_width_at_post"),
+                        rs.getInt("parking_length"),
+                        rs.getInt("parking_width"),
+                        rs.getInt("shed_depth"),
+                        rs.getInt("shed_width"));
+
+            } else
+            {
+                throw new DatabaseException("No Specification found on id " + specificationId);
+            }
+        } catch (SQLException e)
+        {
+            throw new DatabaseException("Error retrieving user", e.getMessage());
+        }
     }
 }

67b3335a2c5ba4f479899fd78a6fdf50488bf47e
frede <frededv@outlook.dk>
2025-11-19 10:15:09 +0000

    User entetie expaneded, constructor finished, method added: getId()

--- src/main/java/app/enteties/User.java
+++ src/main/java/app/enteties/User.java
@@ -1,6 +1,31 @@
 package app.enteties;
 
 public class User {
+int userId;
+String firstName;
+String lastName;
+int zipCode;
+String streetName;
+int houseNumber;
+String floor;
+String email;
+String password;
+int role;
 
-    public User(int userId, String firstName, String lastName, int zip_code, String streetName, int houseNumber, String floor, String email, String password,int role) {}
+    public User(int userId, String firstName, String lastName, int zip_code, String streetName, int houseNumber, String floor, String email, String password,int role) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.zipCode = zip_code;
+        this.streetName = streetName;
+        this.houseNumber = houseNumber;
+        this.floor = floor;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return userId;
+    }
 }

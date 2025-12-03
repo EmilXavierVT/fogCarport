10f4945655e4274b9187d57e2898ac8cc1ce7789
frede <frededv@outlook.dk>
2025-12-03 14:18:48 +0000

    OrderMapper test completed: findOrderTest(); creatOrderTest();

--- src/test/java/app/persistence/OrderMapperTest.java
+++ src/test/java/app/persistence/OrderMapperTest.java
@@ -26,19 +26,29 @@ public class OrderMapperTest {
 
     static ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
+    User tempUser = new User(1,"Emil","Thorsen",2200,"Farumgade",1,"2th","ex@tv.dk","1234",0);
+    Order expectedOrder = new Order (1,tempUser,LocalDate.parse("2025-11-25"));
+
     @BeforeAll
     public static void setUpClass() throws SQLException {
 
         try (Connection connection = connectionPool.getConnection()) {
             try (Statement stmt = connection.createStatement()) {
                 stmt.execute("DROP TABLE IF EXISTS test_schema.orders");
+                stmt.execute("DROP TABLE IF EXISTS test_schema.users");
 
                 stmt.execute("DROP SEQUENCE IF EXISTS test_schema.orders_order_id_seq CASCADE");
+                stmt.execute("DROP SEQUENCE IF EXISTS test_schema.users_user_id_seq CASCADE;");
 
                 stmt.execute("CREATE TABLE test_schema.orders (LIKE public.orders INCLUDING ALL)");
+                stmt.execute("CREATE TABLE test_schema.users AS (SELECT * from public.users) WITH NO DATA");
 
                 stmt.execute("CREATE SEQUENCE test_schema.orders_order_id_seq");
+                stmt.execute("CREATE SEQUENCE test_schema.users_user_id_seq");
+
                 stmt.execute("ALTER TABLE test_schema.orders ALTER COLUMN order_id SET DEFAULT nextval('test_schema.orders_order_id_seq')");
+                stmt.execute("ALTER TABLE test_schema.users ALTER COLUMN user_id SET DEFAULT nextval('test_schema.users_user_id_seq')");
+
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -50,26 +60,40 @@ public class OrderMapperTest {
         try (Connection connection = connectionPool.getConnection()) {
             try (Statement stmt = connection.createStatement()) {
                 stmt.execute("DELETE FROM test_schema.orders");
+                stmt.execute("DELETE FROM test_schema.users");
+
 
                 stmt.execute("SELECT setval('test_schema.orders_order_id_seq',1)");
+                stmt.execute("SELECT setval('test_schema.users_user_id_seq', 1)");
 
                 stmt.execute("INSERT INTO test_schema.orders VALUES " +
                         "(1,1,'2025-11-25')");
+                stmt.execute("INSERT INTO test_schema.orders VALUES " +
+                        "(2,1,'2025-11-28')");
+
+                stmt.execute("INSERT INTO test_schema.users VALUES " +
+                        "(1,'Emil','Thorsen',2200,'Farumgade',1,'2th','ex@tv.dk','1234',0)");
+
                 stmt.execute("SELECT setval('test_schema.orders_order_id_seq', COALESCE((SELECT MAX (order_id)+1 FROM test_schema.orders), 1), false)");
+                stmt.execute("SELECT setval('test_schema.users_user_id_seq', COALESCE((SELECT MAX(user_id)+1 FROM test_schema.users), 1), false)");
+
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
         }
     }
     @Test
-    public void createOrderTest() throws SQLException, DatabaseException {
-        User temp = new User(1,"Emil","Thorsen",2200,"Farumgade",1,"2th","ex@tv.dk","1234",0);
-        Order expectedOrder = new Order(2,temp, LocalDate.parse("2025-11-27"));
-        OrderMapper.saveOrder(temp.getUserId(),LocalDate.parse("2025-11-27"),connectionPool);
-        Order real = OrderMapper.getOrderByID(2,connectionPool);
+    public void findOrderTest(){
+        assertEquals(expectedOrder,OrderMapper.getOrderByID(1,connectionPool));
+    }
 
-        assertEquals(expectedOrder.getId(),real.getId());
-        assertEquals(expectedOrder.getDate(),real.getDate());
-        assertEquals(expectedOrder.getUserId(),real.getUserId());
+    @Test
+    public void createOrderTest() throws SQLException, DatabaseException {
+        assertEquals(2,TestMapper.count("orders",connectionPool));
+        OrderMapper.saveOrder(tempUser.getUserId(),LocalDate.parse("2025-11-27"),connectionPool);
+        assertEquals(3,TestMapper.count("orders",connectionPool));
+
+
+
     }
 }

bbc4b5ad62a6539c13ddadd9a5b15968394b3aa3
luke-dkk <luke_persson@yahoo.dk>
2025-12-03 12:40:40 +0100

    Made base for method "showAllCarports"

--- src/main/java/app/controllers/AdminController.java
+++ src/main/java/app/controllers/AdminController.java
@@ -1,12 +1,35 @@
 package app.controllers;
 
+import app.entities.Carport;
+import app.exceptions.DatabaseException;
+import app.persistence.CarportMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+import java.util.Map;
 
 public class AdminController {
     public static void addRoutes(Javalin app) {
         ConnectionPool connectionPool = ConnectionPool.getInstance();
+
         app.get("/admin/alert", ctx -> ctx.render("admin/alert.html"));
-        
+
+    }
+
+    public static void showAllCarports (Context ctx, ConnectionPool connectionPool) throws DatabaseException
+    {
+        try
+        {
+            List<Carport> standardCarports = CarportMapper.getAllStandardCarport(connectionPool);
+
+            ctx.render("index.html", Map.of("standard_carports", standardCarports));
+
+        } catch (DatabaseException e)
+        {
+            System.out.println("showCarport signature: Could not get all standard carports" + e.getMessage());
+            ctx.redirect("/");
+        }
     }
 }

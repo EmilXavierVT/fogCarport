1b4c8a8cec50bdf405f41138b30c382384dffcda
luke-dkk <luke_persson@yahoo.dk>
2025-12-03 15:41:13 +0100

    made adminController, and created showAllOrders, showAllCarports and showAllMaterials methods

--- src/main/java/app/controllers/AdminController.java
+++ src/main/java/app/controllers/AdminController.java
@@ -1,11 +1,14 @@
 package app.controllers;
 
 import app.entities.Carport;
+import app.entities.CarportRequest;
+import app.entities.Order;
+import app.entities.Product;
 import app.exceptions.DatabaseException;
-import app.persistence.CarportMapper;
-import app.persistence.ConnectionPool;
+import app.persistence.*;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
@@ -14,9 +17,29 @@ public class AdminController {
     public static void addRoutes(Javalin app) {
         ConnectionPool connectionPool = ConnectionPool.getInstance();
 
-        app.get("/admin/alert", ctx -> ctx.render("admin/alert.html"));
+        app.get("/admin/alert", ctx -> {showAllCarports(ctx, connectionPool);
+            showAllMaterials(ctx, connectionPool);
+        showAllOrders(ctx, connectionPool);});
+
 
     }
+
+    private static void showAllOrders(Context ctx, ConnectionPool connectionPool)
+    {
+        try
+        {
+            List<CarportRequest> orders = CarportRequestMapper.getAllCarportRequests(connectionPool);
+
+            ctx.render("admin/alert.html", Map.of("all_carport_requests", orders));
+
+        } catch (DatabaseException e)
+        {
+            System.out.println("showAllMaterials signature: Could not get all materials" + e.getMessage());
+            ctx.redirect("/");
+        }
+    }
+
+
 
     public static void showAllCarports (Context ctx, ConnectionPool connectionPool) throws DatabaseException
     {
@@ -24,11 +47,26 @@ public class AdminController {
         {
             List<Carport> standardCarports = CarportMapper.getAllStandardCarport(connectionPool);
 
-            ctx.render("index.html", Map.of("standard_carports", standardCarports));
+            ctx.render("admin/alert.html", Map.of("all_standard_carports", standardCarports));
 
         } catch (DatabaseException e)
         {
-            System.out.println("showCarport signature: Could not get all standard carports" + e.getMessage());
+            System.out.println("showAllCarport signature: Could not get all carports" + e.getMessage());
+            ctx.redirect("/");
+        }
+    }
+
+    public static void showAllMaterials (Context ctx, ConnectionPool connectionPool) throws DatabaseException
+    {
+        try
+        {
+            List<Product> products = ProductMapper.getAllProducts(connectionPool);
+
+            ctx.render("admin/alert.html", Map.of("all_materials", products));
+
+        } catch (DatabaseException e)
+        {
+            System.out.println("showAllMaterials signature: Could not get all materials" + e.getMessage());
             ctx.redirect("/");
         }
     }

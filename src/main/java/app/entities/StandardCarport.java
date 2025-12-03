3346eda895ad32fe12549dc2d502d057fcca757e
luke-dkk <luke_persson@yahoo.dk>
2025-12-03 12:42:18 +0100

    Made overloaded constructor for StandardCarport

--- src/main/java/app/entities/StandardCarport.java
+++ src/main/java/app/entities/StandardCarport.java
@@ -9,11 +9,16 @@ float price;
 int type;
 String description;
 int specification;
-
 String pdfFile;
+
+
     public StandardCarport(int standardId, String name, float price, int type, String description, int specification, String pdf_file)
     {
         super(standardId,name,price,type,description,specification);
         this.pdfFile = pdf_file;
+    }
+    public StandardCarport(int standardId, String name, float price, int type)
+    {
+        super(standardId,name,price,type);
     }
 }

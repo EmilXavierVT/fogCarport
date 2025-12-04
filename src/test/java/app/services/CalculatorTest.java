de1a71bd3428083283eec372055f8bc28487832a
frede <frededv@outlook.dk>
2025-12-04 10:36:34 +0000

    Syntax fixes: UserMapperTest CalculatorTest

--- src/test/java/app/services/CalculatorTest.java
+++ src/test/java/app/services/CalculatorTest.java
@@ -2,47 +2,39 @@ package app.services;
 
 import app.entities.ProductInOrder;
 import app.entities.Specification;
-import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.SpecificationMapper;
-import app.persistence.UserMapper;
 import io.github.cdimascio.dotenv.Dotenv;
-import io.javalin.http.Context;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
-import org.mockito.Mockito;
-
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CalculatorTest {
+class CalculatorTest
+{
     private final static Dotenv dotenv = Dotenv.load();
     private final static String USER = dotenv.get("DB-USER");
     private final static String PASSWORD = dotenv.get("DB-PASSWORD");
     private final static String URL = dotenv.get("DB-URL");
     private final static String DB = "carport";
-
     static ConnectionPool connectionPool = ConnectionPool.getInstance(USER,PASSWORD,URL,DB);
 
     @BeforeAll
-    public static void setUpClass() {
-
-        try (Connection connection = connectionPool.getConnection()) {
-            try (Statement stmt = connection.createStatement()) {
+    public static void setUpClass()
+    {
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (Statement stmt = connection.createStatement())
+            {
                 stmt.execute("DROP TABLE IF EXISTS test_schema.specifications");
                 stmt.execute("DROP TABLE IF EXISTS test_schema.products");
 
                 stmt.execute("DROP SEQUENCE IF EXISTS test_schema.specifications_specification_id_seq CASCADE");
                 stmt.execute("DROP SEQUENCE IF EXISTS test_schema.products_product_id_seq CASCADE");
 
-                //stmt.execute("CREATE TABLE test_schema.carports AS (SELECT * FROM public.carports) WITH NO DATA");
                 stmt.execute("CREATE TABLE test_schema.specifications (LIKE public.specifications INCLUDING ALL)");
                 stmt.execute("CREATE TABLE test_schema.products (LIKE public.products INCLUDING ALL)");
 
@@ -52,14 +44,19 @@ class CalculatorTest {
                 stmt.execute("ALTER TABLE test_schema.specifications ALTER COLUMN specification_id SET DEFAULT nextval('test_schema.specifications_specification_id_seq')");
                 stmt.execute("ALTER TABLE test_schema.products ALTER COLUMN product_id SET DEFAULT nextval('test_schema.products_product_id_seq')");
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new RuntimeException(e);
         }
     }
     @BeforeEach
-    void setUp() throws SQLException {
-        try (Connection connection = connectionPool.getConnection()) {
-            try (Statement stmt = connection.createStatement()) {
+    void setUp() throws SQLException
+    {
+        try (Connection connection = connectionPool.getConnection())
+        {
+            try (Statement stmt = connection.createStatement())
+            {
                 stmt.execute("DELETE FROM test_schema.specifications");
                 stmt.execute("DELETE FROM test_schema.products");
 
@@ -71,7 +68,6 @@ class CalculatorTest {
 
                 stmt.execute("INSERT INTO test_schema.products VALUES " +
                         "(22,'trykimprægneret stolpe','125x125 mm','stolper graves 90 cm i jord',110,22)");
-
                 stmt.execute("INSERT INTO test_schema.products VALUES " +
                         "(3,'spærtræ','45x195 mm','Remme i sider, sadles ned i stolper',100,3)");
                 stmt.execute("INSERT INTO test_schema.products VALUES " +
@@ -84,28 +80,31 @@ class CalculatorTest {
                         "(9,'vægbeslag','50x50 mm','beslag til væg',20,9)");
 
                 stmt.execute("SELECT setval('test_schema.specifications_specification_id_seq', COALESCE((SELECT MAX (specification_id)+1 FROM test_schema.specifications), 1), false)");
-            } catch (SQLException e) {
+            }
+            catch (SQLException e)
+            {
                 throw new RuntimeException(e);
             }
         }
     }
 
     @Test
-    public void testCalculatePrice() throws DatabaseException, SQLException {
+    public void testCalculatePrice() throws DatabaseException, SQLException
+    {
         Specification testSpecification = SpecificationMapper.getSpecificationByID(1,connectionPool);
         Calculator calc = new Calculator(testSpecification);
         calc.setConnectionPool(connectionPool);
-
         System.out.println(calc.getCostPrice());
     }
     @Test
-    public void testItemList() throws SQLException, DatabaseException {
+    public void testItemList() throws SQLException, DatabaseException
+    {
         Specification testSpec = SpecificationMapper.getSpecificationByID(1,connectionPool);
         Calculator calc = new Calculator(testSpec);
+
        for(ProductInOrder pio : calc.setItemList())
        {
            System.out.println(pio + "\n");
        }
-
     }
 }

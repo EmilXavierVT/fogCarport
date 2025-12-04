21e74563f03d0fe0c4d3ba181b545c7fa0f7ca1e
DannyShayH <HalawiDD@gmail.com>
2025-12-04 17:53:39 +0100

    Created cart overlay, added into fragments and products pages. Added javascript for the overlay (view item, that was put in cart), added css for the overlay and gave a href to cart icon

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -22,3 +22,46 @@ document.querySelectorAll(".product_info_box").forEach(box =>{
     box.classList.toggle("open");
    });
 });
+
+//CART OVERLAY
+function showCartOverlay(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const amount = form.querySelector("input[name='amount']").value;
+    const productName = form.querySelector("input[name='productName']").value;
+    const thumb = form.querySelector("input[name='productThumbnail']").value;
+    const price = form.querySelector("input[name='productPrice']").value;
+
+    const overlay = document.getElementById("cart_overlay");
+    const nameEl = document.getElementById("overlay_product_name");
+    const amountEl = document.getElementById("overlay_product_amount");
+    const overlayImage = document.getElementById("cart_overlay_image");
+    const priceEl = document.getElementById("overlay_product_price");
+
+    nameEl.textContent = productName;
+    amountEl.textContent = "Antal: " + amount;
+    priceEl.textContent = "Pris: " + (price * amount) + " kr.";
+    overlayImage.src = thumb;
+
+    document.getElementById("overlay_input_productName").value = productName;
+    document.getElementById("overlay_input_amount").value = amount;
+    document.getElementById("overlay_input_thumbnail").value = thumb;
+    document.getElementById("overlay_input_price").value = price;
+
+    overlay.classList.remove("hidden");
+    setTimeout(() => overlay.classList.add("show"), 10);
+
+    const closeButton = document.getElementById("cart_overlay_close");
+    closeButton.onclick = () => {
+      overlay.classList.remove("show");
+      setTimeout(() => overlay.classList.add("hidden"), 350);
+    };
+
+    const removeButton = document.getElementById("overlay_remove_item");
+    removeButton.onclick = () => {
+        overlay.classList.remove("show");
+        setTimeout(() => overlay.classList.add("hidden"));
+    };
+}
+

48d71219ec1bc4461db33ecd12bfe5dba5fc6184
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-12-08 18:29:05 +0000

    madde angle page and controler now working on how to calculate the new angle roofs

--- src/main/java/app/controllers/UserDefinedController.java
+++ src/main/java/app/controllers/UserDefinedController.java
@@ -14,7 +14,33 @@ public class UserDefinedController {
        app.get("userdefined",ctx -> ctx.render("userdefined.html"));
        app.get("flat",ctx->ctx.render("flat.html"));
        app.post("flat",ctx-> sendRequest(ctx,connectionPool));
+       app.get("/angle", ctx -> ctx.render("angle"));
+       app.post("/angle", ctx -> sendAngleRequest(ctx, connectionPool));
 
+    }
+
+    private static void sendAngleRequest( Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        int width = Integer.parseInt(ctx.formParam("width"));
+        int length = Integer.parseInt(ctx.formParam("length"));
+        String roofType = ctx.formParam("roof");
+        int angle = Integer.parseInt(ctx.formParam("angle"));
+        int shedWidth = Integer.parseInt(ctx.formParam("shed_width"));
+        int shedLength = Integer.parseInt(ctx.formParam("shed_length"));
+        String remarks = ctx.formParam("remarks");
+        String name = ctx.formParam("name");
+        String address = ctx.formParam("address");
+        int zipCode = Integer.parseInt(ctx.formParam("zip_code"));
+        String city = ctx.formParam("city");
+        String phoneNumber = ctx.formParam("phone_number");
+        String email = ctx.formParam("email");
+
+        boolean roof = !roofType.equals("Ingen tag");
+
+
+
+        Calculator calculator = new Calculator(SpecificationWizard.makeASpecification(width,length,roof,shedWidth,shedLength));
+
+        System.out.println(calculator.setItemList());
     }
 
     private static void sendRequest(Context ctx, ConnectionPool connectionPool) throws DatabaseException {

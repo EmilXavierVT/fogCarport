b8783df61f9be90ebc78a3c099d7b902a3b41032
Freddyboi123 <Frededv@outlook.dk>
2025-12-17 13:15:44 +0100

    Final syntax fixes CarportRequestMapper

--- src/main/java/app/persistence/CarportRequestMapper.java
+++ src/main/java/app/persistence/CarportRequestMapper.java
@@ -9,37 +9,44 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarportRequestMapper {
+public class CarportRequestMapper
+{
     public static void createCarportRequest(int userID, int carportID, int salesRepID, ConnectionPool connectionPool) throws DatabaseException {
         String sql = "INSERT INTO carport_requests (user_id, carport_id, sales_rep_id) VALUES (?, ?, ?)";
 
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql);) {
+             PreparedStatement ps = connection.prepareStatement(sql);)
+        {
             ps.setInt(1, userID);
             ps.setInt(2, carportID);
             ps.setInt(3, salesRepID);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException(e.getMessage() + "problem with saving carport request");
         }
     }
-
 
     public static CarportRequest getCarportByRequestID(int id, ConnectionPool connectionPool) throws DatabaseException, SQLException {
         String sql = "SELECT * FROM carport_requests WHERE carport_request_id = ?";
 
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
-            while (rs.next()) {
+            while (rs.next())
+            {
                 return new CarportRequest(rs.getInt(1),
                         UserMapper.getUserByID(rs.getInt(2), connectionPool),
                         CarportMapper.getCarportByID(rs.getInt(3), connectionPool),
                         UserMapper.getUserByID(rs.getInt(4), connectionPool));
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException(e.getMessage() + "problem with getting a carport request by id: " + id);
         }
         return null;
@@ -49,62 +56,75 @@ public class CarportRequestMapper {
         String sql = "UPDATE carport_requests SET user_id = ?, carport_id = ?, sales_rep_id = ? WHERE carport_request_id = ?";
 
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql);) {
+             PreparedStatement ps = connection.prepareStatement(sql);)
+        {
             ps.setInt(1, userId);
             ps.setInt(2, carportId);
             ps.setInt(3, salesRepId);
             ps.setInt(4, requestId);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException(e.getMessage() + "problem with updating carport request with id: " + requestId);
         }
     }
 
-    public static void deleteCarportRequest(int id, ConnectionPool connectionPool) throws DatabaseException {
+    public static void deleteCarportRequest(int id, ConnectionPool connectionPool) throws DatabaseException
+    {
         String sql = "DELETE FROM carport_requests WHERE carport_request_id = ?";
 
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql);) {
+             PreparedStatement ps = connection.prepareStatement(sql);)
+        {
             ps.setInt(1, id);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException(e.getMessage() + "problem with deleting carport request with id: " + id);
         }
     }
 
-    public static List<CarportRequest> getAllCarportRequests(ConnectionPool connectionPool) throws DatabaseException {
+    public static List<CarportRequest> getAllCarportRequests(ConnectionPool connectionPool) throws DatabaseException
+    {
         List<CarportRequest> requests = new ArrayList<>();
         String sql = "SELECT * FROM carport_requests";
 
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql)) {
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
             ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
+            while (rs.next())
+            {
                 requests.add(new CarportRequest(
                         rs.getInt("carport_request_id"),
                         UserMapper.getUserByID(rs.getInt("user_id"), connectionPool),
                         CarportMapper.getCarportByID(rs.getInt("carport_id"), connectionPool),
                         rs.getInt("sales_rep_id") != 0 ? UserMapper.getUserByID(rs.getInt("sales_rep_id"), connectionPool) : null,
                         rs.getInt("status")
-
                 ));
             }
             return requests;
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new DatabaseException(e.getMessage() + " problem with getting all carport requests");
         }
     }
 
-    public static void updateStatus(int id, int status, ConnectionPool connectionPool) {
+    public static void updateStatus(int id, int status, ConnectionPool connectionPool)
+    {
         try (Connection connection = connectionPool.getConnection();
-             PreparedStatement ps = connection.prepareStatement("UPDATE carport_requests SET status = ? WHERE carport_request_id = ?")) {
+             PreparedStatement ps = connection.prepareStatement("UPDATE carport_requests SET status = ? WHERE carport_request_id = ?"))
+        {
             ps.setInt(1, status);
             ps.setInt(2, id);
             ps.executeUpdate();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
-
     }
-
 }

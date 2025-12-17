01c891e6d91b0954a699e56aaf497bd47e1db26a
Freddyboi123 <Frededv@outlook.dk>
2025-12-17 13:13:14 +0100

    Final syntax fixes UserDefinedCarport

--- src/main/java/app/entities/UserDefinedCarport.java
+++ src/main/java/app/entities/UserDefinedCarport.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class UserDefinedCarport extends Carport
 {
-
     List itemList = new ArrayList();
 
     public UserDefinedCarport(int carportID, String name, float price, int type, String productionDescription, Specification specification)

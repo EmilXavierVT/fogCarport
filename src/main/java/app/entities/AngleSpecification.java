19ce553d23f1d9c18bf11d5f7af346dff5de34f8
Freddyboi123 <Frededv@outlook.dk>
2025-12-17 13:10:46 +0100

    Final syntax fixes AngelSpecifications

--- src/main/java/app/entities/AngleSpecification.java
+++ src/main/java/app/entities/AngleSpecification.java
@@ -1,10 +1,11 @@
 package app.entities;
 
-public class AngleSpecification extends Specification {
+public class AngleSpecification extends Specification
+{
    private int angle;
 
-
-    public AngleSpecification(long EAN, String model, int roomFor, boolean shed, Product post, Product beam, Product rafter, Product roof, Product fasciaBoard, int length, int width, int heightFront, int heightRear, int roofLength, int roofWidth, int exteriorWidthAtPost, int parkingLength, int parkingWidth, int shedDepth, int shedWidth, int angle) {
+    public AngleSpecification(long EAN, String model, int roomFor, boolean shed, Product post, Product beam, Product rafter, Product roof, Product fasciaBoard, int length, int width, int heightFront, int heightRear, int roofLength, int roofWidth, int exteriorWidthAtPost, int parkingLength, int parkingWidth, int shedDepth, int shedWidth, int angle)
+    {
         super(EAN, model, roomFor, shed, post, beam, rafter, roof, fasciaBoard, length, width, heightFront, heightRear, roofLength, roofWidth, exteriorWidthAtPost, parkingLength, parkingWidth, shedDepth, shedWidth);
         this.angle = angle;
     }
